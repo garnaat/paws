e9df553d0ad35f5f50aadfdd2110ec5e22793a2f
Mitch Garnaat <mitch@garnaat.com>
2011-12-09 12:08:49 -0500

    Fixed preserve_acl param and added additional metadata param.  Closes #2.

--- s3_copy_object.py
+++ s3_copy_object.py
@@ -4,7 +4,8 @@ def copy_object(src_bucket_name,
                 src_key_name,
                 dst_bucket_name,
                 dst_key_name,
-                preserve_metadata=True):
+                metadata=None,
+                preserve_acl=True):
     """
     Copy an existing object to another location.
 
@@ -12,6 +13,10 @@ def copy_object(src_bucket_name,
     src_key_name      Name of the existing object.
     dst_bucket_name   Bucket to which the object is being copied.
     dst_key_name      The name of the new object.
+    metadata          A dict containing new metadata that you want
+                      to associate with this object.  If this is None
+                      the metadata of the original object will be
+                      copied to the new object.
     preserve_acl      If True, the ACL from the original object
                       will be copied to the new object.  If False
                       the new object will have the default ACL.
@@ -23,7 +28,8 @@ def copy_object(src_bucket_name,
     key = bucket.lookup(src_key_name)
 
     # Copy the key back on to itself, with new metadata
-    return key.copy(dst_bucket_name, dst_key_name, preserve_acl=preserve_acl)
+    return key.copy(dst_bucket_name, dst_key_name,
+                    metadata=metadata, preserve_acl=preserve_acl)
 
                        
 
