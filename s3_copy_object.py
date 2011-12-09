5e32df202ef101ea82e76ed7c4a28bf26485feeb
Mitch Garnaat <mitch@garnaat.com>
2011-12-09 08:09:31 -0500

    Changing preserve_acl parameter of copy_object method to be preserve_acl.  Also changing the docstring.  Closes #2.

--- s3_copy_object.py
+++ s3_copy_object.py
@@ -12,9 +12,9 @@ def copy_object(src_bucket_name,
     src_key_name      Name of the existing object.
     dst_bucket_name   Bucket to which the object is being copied.
     dst_key_name      The name of the new object.
-    preserve_metadata If True, all metadata on the original object
-                      will be preserved on the new object.  If False
-                      the new object will have the default metadata.
+    preserve_acl      If True, the ACL from the original object
+                      will be copied to the new object.  If False
+                      the new object will have the default ACL.
     """
     s3 = boto.connect_s3()
     bucket = s3.lookup(src_bucket_name)
